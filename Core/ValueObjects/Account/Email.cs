6e341fcd544be02207b3816c64fd91375e72b185
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 15:48:57 +0700

    feat<core>: create `Email`

---
+++ Core/ValueObjects/Account/Email.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+using Core.Exceptions;
+
+namespace Core.ValueObjects.Account;
+
+public readonly struct Email
+{
+   public Email(string value)
+   {
+      if (!IsValid(value))
+      {
+         throw new InvalidEmailException(value);
+      }
+      Value = value;
+   }
+
+   #region static
+
+   private static readonly Regex EmailRegex = new Regex("^\\S+@\\S+\\.\\S+$");
+
+   public static bool IsValid(string value)
+   {
+      return EmailRegex.IsMatch(value);
+   }
+
+   #endregion
+
+   private string Value { get; init; }
+
+   public override string ToString()
+   {
+      return Value;
+   }
+
+   public static explicit operator string(Email email)
+   {
+      return email.Value;
+   }
+
+   public static explicit operator Email(string emailString)
+   {
+      return new Email(emailString);
+   }
+}
