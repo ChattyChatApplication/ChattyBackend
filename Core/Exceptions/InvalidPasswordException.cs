1cf4bffdff57dd2c2c5ac0597c1baff37e4e9328
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 15:45:34 +0700

    feat<core>: create `InvalidPasswordException`

---
+++ Core/Exceptions/InvalidPasswordException.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Exceptions;
+
+public class InvalidPasswordException : Exception
+{
+   public InvalidPasswordException(string password) : base(password)
+   {
+   }
+
+   public override string Message => $"{base.Message} is an invalid password";
+}
