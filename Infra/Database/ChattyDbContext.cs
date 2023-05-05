f7dbeccf0d3824fe0f47125ad97318aca835e50e
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 17:33:07 +0700

    feat<infra>: create `ChattyDbContext`

---
+++ Infra/Database/ChattyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using Infra.Database.EntityConfigs;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Database;
+
+public class ChattyDbContext : DbContext
+{
+   public DbSet<UserAccount> UserAccounts { get; set; } = null!;
+   
+   public ChattyDbContext(DbContextOptions options) : base(options)
+   {
+   }
+
+   protected override void OnModelCreating(ModelBuilder modelBuilder)
+   {
+      modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserAccountConfig).Assembly);
+   }
+}
