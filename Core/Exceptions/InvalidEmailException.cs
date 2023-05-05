e544360ef0be39937cbc42ce4b0b3949d21fd701
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 15:48:32 +0700

    feat<core>: create `InvalidEmailException`

---
+++ Core/Exceptions/InvalidEmailException.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Exceptions;
+
+public class InvalidEmailException : Exception
+{
+   public InvalidEmailException(string? email) : base(email)
+   {
+   }
+
+   public override string Message => $"{base.Message} is an invalid email";
+}
