e55136490789139bd33b8fbb7dc73f17e12bedc0
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 16:05:56 +0700

    feat<core>: create `UserAccount` entity

---
+++ Core/Entities/UserAccount.cs
@@ -0,0 +1,19 @@
+﻿using Core.ValueObjects.Account;
+
+namespace Core.Entities;
+
+public class UserAccount : BaseEntity
+{
+   public UserAccount(Guid id, Username username, Email email, PasswordHash passwordHash) : base(id)
+   {
+      Username = username;
+      Email = email;
+      PasswordHash = passwordHash;
+   }
+
+   public Username Username { get; init; }
+   
+   public Email Email { get; init; }
+
+   public PasswordHash PasswordHash { get; set; }
+}
