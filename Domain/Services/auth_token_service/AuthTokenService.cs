8db8ffbc8c3ac8fe42f5a0765f6e2dd5b5cd060e
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-06 02:41:29 +0700

    feat<domain>: create `AuthTokenService` within `CreateAuthToken`

---
+++ Domain/Services/auth_token_service/AuthTokenService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+
+namespace Domain.Services;
+
+public class AuthTokenService : IAuthTokenService
+{
+   public AuthToken CreateAuthToken(UserAccount userAccount)
+   {
+      // TODO implement logic here
+      return new AuthToken(
+         accessToken: "this is access token",
+         refreshToken: "this is refresh token token"
+      );
+   }
+}
---
+++ Domain/Services/auth_token_service/IAuthTokenService.cs
@@ -0,0 +1,8 @@
+﻿using Core.Entities;
+
+namespace Domain.Services;
+
+public interface IAuthTokenService
+{
+   public AuthToken CreateAuthToken(UserAccount userAccount);
+}
