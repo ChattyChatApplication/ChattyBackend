ce7797610613ac78f8ad9de4d202d72070b0fd1b
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 17:31:28 +0700

    feat<infra>: create `UserAccountConfig`

---
+++ Infra/Database/EntityConfigs/UserAccountConfig.cs
@@ -0,0 +1,71 @@
+﻿using Core.Entities;
+using Core.ValueObjects.Account;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Database.EntityConfigs;
+
+public class UserAccountConfig : IEntityTypeConfiguration<UserAccount>
+{
+   public void Configure(EntityTypeBuilder<UserAccount> builder)
+   {
+      #region indexs
+
+      builder
+         .HasKey(uac => uac.Id);
+
+      builder
+         .HasIndex(uac => uac.Username)
+         .IsUnique();
+      
+      builder
+         .HasIndex(uac => uac.Email)
+         .IsUnique();
+
+      #endregion
+
+      #region columns
+      
+      builder
+         .Property(uac => uac.Username)
+         .HasConversion(
+            username => username.ToString(),
+            value => new Username(value)
+         )
+         .HasColumnType("varchar(30)")
+         // TODO limit min length to 2
+         .IsRequired();
+
+      builder
+         .Property(uac => uac.Email)
+         .HasConversion(
+            email => email.ToString(),
+            value => new Email(value)
+         )
+         .HasColumnType("varchar")
+         .IsRequired();
+      
+      builder
+         .Property(uac => uac.PasswordHash)
+         .HasConversion(
+            email => email.ToString(),
+            value => new PasswordHash(value)
+         )
+         .HasColumnType("varchar")
+         .IsRequired();
+
+      #endregion
+
+      #region auto generates
+
+      builder
+         .Property(ett => ett.Inserted)
+         .ValueGeneratedOnAdd();
+      
+      builder
+         .Property(ett => ett.Updated)
+         .ValueGeneratedOnAddOrUpdate();
+
+      #endregion
+   }
+}
