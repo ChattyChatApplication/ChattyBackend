ad7056d361e4b933b9245588d3b9c741e8dc18c0
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 15:35:30 +0700

    feat<core>: create `InvalidUsernameException`

---
+++ Core/Exceptions/InvalidUsernameException.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Exceptions;
+
+public class InvalidUsernameException : Exception
+{
+   public InvalidUsernameException(string username) : base(username)
+   {
+   }
+
+   public override string Message => $"{base.Message} is an invalid username";
+}
