80b99ed5fe5732e99ba93e9a135755c28fb9a9cb
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 16:42:56 +0700

    feat<domain>: create `DuplicatedEmailException`

---
+++ Domain/Exceptions/DuplicatedEmailException.cs
@@ -0,0 +1,12 @@
+﻿using Core.ValueObjects.Account;
+
+namespace Domain.Exceptions;
+
+public class DuplicatedEmailException : Exception
+{
+   public DuplicatedEmailException(Email email) : base(email.ToString())
+   {
+   }
+
+   public override string Message => $"{base.Message} is already exist";
+}
