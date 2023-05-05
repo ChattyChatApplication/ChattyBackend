205e2bfb1aaaaa7ec07d72298380704436bcd43e
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 21:45:00 +0700

    feat<infra>: implement `UserAccountRepository`

---
+++ Infra/Database/Repositories/UserAccountRepository.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using Core.ValueObjects.Account;
+using Domain.Contracts.Database.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Database.Repositories;
+
+public class UserAccountRepository : BaseRepository<UserAccount>, IUserAccountRepository
+{
+   public UserAccountRepository(ChattyDbContext dbContext) : base(dbContext)
+   {
+   }
+
+   public async Task<bool> IsUsernameExistAsync(Username username)
+   {
+      var matchingUsername = await DataAccess
+         .SingleOrDefaultAsync(uac => uac.Username == username);
+
+      return matchingUsername is not null;
+   }
+
+   public async Task<bool> IsEmailExistAsync(Email email)
+   {
+      var matchingEmail = await DataAccess
+         .SingleOrDefaultAsync(uac => uac.Email == email);
+
+      return matchingEmail is not null;
+   }
+}
