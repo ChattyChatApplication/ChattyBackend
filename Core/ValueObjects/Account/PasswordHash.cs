bf751ba9500f1fdd3d406495daebeb32eb7b6edc
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 15:54:31 +0700

    feat<core>: create `PasswordHash`

---
+++ Core/ValueObjects/Account/PasswordHash.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+using Core.Exceptions;
+
+namespace Core.ValueObjects.Account;
+
+public readonly struct PasswordHash
+{
+   public PasswordHash(string value)
+   {
+      if (!IsValid(value))
+      {
+         throw new InvalidPasswordHashException(value);
+      }
+      Value = value;
+   }
+
+   #region static
+
+   private static readonly Regex PasswordHashRegex = new Regex(".");
+
+   public static bool IsValid(string value)
+   {
+      return PasswordHashRegex.IsMatch(value);
+   }
+
+   #endregion
+
+   private string Value { get; init; }
+
+   public override string ToString()
+   {
+      return Value;
+   }
+
+   public static explicit operator string(PasswordHash passwordHash)
+   {
+      return passwordHash.Value;
+   }
+
+   public static explicit operator PasswordHash(string passwordHashString)
+   {
+      return new PasswordHash(passwordHashString);
+   }
+}
