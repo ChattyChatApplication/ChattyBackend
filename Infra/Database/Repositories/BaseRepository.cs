f720b0580f87ede2092e5835cb69c230496bc691
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 21:27:18 +0700

    feat<infra>: implement `BaseRepository` and `InsertAsync`

---
+++ Infra/Database/Repositories/BaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Domain.Contracts.Database.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Database.Repositories;
+
+public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+{
+   private readonly ChattyDbContext _dbContext;
+   private readonly DbSet<T> _userAccounts;
+
+   public BaseRepository(ChattyDbContext dbContext)
+   {
+      _dbContext = dbContext;
+      _userAccounts = _dbContext.Set<T>();
+   }
+
+   public async Task InsertAsync(T entity)
+   {
+      await _userAccounts.AddAsync(entity);
+      await _dbContext.SaveChangesAsync();
+   }
+}
