1a03d18722ea9ea84510323683204349a7ddf4c8
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 16:44:34 +0700

    feat<domain>: create `DuplicatedUsernameException`

---
+++ Domain/Exceptions/DuplicatedUsernameException.cs
@@ -0,0 +1,12 @@
+﻿using Core.ValueObjects.Account;
+
+namespace Domain.Exceptions;
+
+public class DuplicatedUsernameException : Exception
+{
+   public DuplicatedUsernameException(Username username) : base(username.ToString())
+   {
+   }
+
+   public override string Message => $"{base.Message} is already exist";
+}
