645f99c2cb966ec8ffc6a1bad29d3bfc20bf9d1b
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 17:53:23 +0700

    feat<api>: create `AddAllDependencies`

---
+++ Api/Configs/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Infra.Configs;
+
+namespace Api.Configs;
+
+public static class DependencyInjection
+{
+   public static void AddAllDependencies(this IServiceCollection services, IConfiguration configuration)
+   {
+      services.AddControllers();
+      // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+      services.AddEndpointsApiExplorer();
+      services.AddSwaggerGen();      
+      
+      services.AddInfra(configuration);
+   }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,12 +1,9 @@
+using Api.Configs;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAllDependencies(builder.Configuration);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+// TODO create AppConfig and put all these shits below inside
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
