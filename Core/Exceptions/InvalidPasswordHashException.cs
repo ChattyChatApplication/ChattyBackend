394c6e15e4de968382390f881dbbbb586a62ce07
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 15:53:03 +0700

    feat<core>: create `InvalidPasswordHashException`

---
+++ Core/Exceptions/InvalidPasswordHashException.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Exceptions;
+
+public class InvalidPasswordHashException : Exception
+{
+   public InvalidPasswordHashException(string? passwordHash) : base(passwordHash)
+   {
+   }
+
+   public override string Message => $"{base.Message} is an invalid password hash";
+}
