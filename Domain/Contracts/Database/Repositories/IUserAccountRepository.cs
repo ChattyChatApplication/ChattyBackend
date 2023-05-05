e4035cf24512f636c0245d8aa88c2edc12cf1b01
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-05 16:27:10 +0700

    feat<domain>: create `IUserAccountRepository`

---
+++ Domain/Contracts/Database/Repositories/IUserAccountRepository.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities;
+using Core.ValueObjects.Account;
+
+namespace Domain.Contracts.Database.Repositories;
+
+public interface IUserAccountRepository : IBaseRepository<UserAccount>
+{
+   public bool IsUsernameExistAsync(Username email);
+
+   public bool IsEmailExistAsync(Email email);
+}
