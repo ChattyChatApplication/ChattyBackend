46b72bab22b445e60c0da5b96a3b2de521e6e287
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-06 07:10:56 +0700

    feat<app>: create `SignUpRequestValidator`

---
+++ App/Validations/SignUpRequestValidator.cs
@@ -0,0 +1,31 @@
+﻿using App.Dtos.Requests;
+using Core.ValueObjects.Account;
+using Domain.Contracts.Database.Repositories;
+using FluentValidation;
+
+namespace App.Validations;
+
+public class SignUpRequestValidator : AbstractValidator<SignUpRequestDto>
+{
+   [Obsolete("Obsolete")]
+   public SignUpRequestValidator(IUserAccountRepository userAccountRepository)
+   {
+      RuleFor(sur => sur.Username)
+         .Cascade(CascadeMode.StopOnFirstFailure)
+         .Must(Username.IsValid)
+         .WithMessage(Username.MessageRegex)
+         .MustAsync(async (usn, _) => !await userAccountRepository.IsUsernameExistAsync((Username)usn))
+         .WithMessage("username is duplicate");
+
+      RuleFor(sur => sur.Email)
+         .Cascade(CascadeMode.StopOnFirstFailure)
+         .Must(Email.IsValid)
+         .WithMessage("invalid email format")
+         .MustAsync(async (eml, _) => !await userAccountRepository.IsEmailExistAsync((Email)eml))
+         .WithMessage("email is duplicated");
+
+      RuleFor(sur => sur.Password)
+         .Must(Password.IsValid)
+         .WithMessage(Password.RegexMessage);
+   }
+}
