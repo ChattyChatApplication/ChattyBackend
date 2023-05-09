db9da1939bae0d57c15f5b347bab44e0c8eda702
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-09 08:56:01 +0700

    feat<core>: add `UserProfile` properties

--- Core/Entities/UserProfile.cs
+++ Core/Entities/UserProfile.cs
@@ -1,10 +1,28 @@
-﻿namespace Core.Entities;
+﻿using Core.ValueObjects.UserProfile;
+using Path = Core.ValueObjects.Commons.Path;
+
+namespace Core.Entities;
 
 public class UserProfile : BaseEntity
 {
+   public UserProfile(Name fullname, Path avatarUri, Guid accountId)
+   {
+      Fullname = fullname;
+      AvatarUri = avatarUri;
+      AccountId = accountId;
+   }
+
+   public UserProfile(Name fullname, Path avatarUri)
+   {
+      Fullname = fullname;
+      AvatarUri = avatarUri;
+   }
+
    #region columns
 
-   
+   public Name Fullname { get; set; }
+
+   public Path AvatarUri { get; set; }
 
    #endregion
 
