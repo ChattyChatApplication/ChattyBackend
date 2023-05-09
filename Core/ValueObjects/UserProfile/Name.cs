b66dd556dab6375ab43057ab1b632ad49963e623
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-09 07:03:23 +0700

    feat<core>: create `Name` value object

---
+++ Core/ValueObjects/UserProfile/Name.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Core.ValueObjects.UserProfile;
+
+public readonly struct Name
+{
+   public string Value { get; init; }
+
+   public Name(string value) => Value = value;
+
+   #region static
+   
+   /// <summary>
+   /// - does not start with a character.
+   /// - have at least one char.
+   /// - does not contain number.
+   /// </summary>
+   public static readonly Regex NameRegex = new(@"^(?! )(?!\s$)[^\d\s]+$");
+
+   public static bool IsValid(string value) => NameRegex.IsMatch(value);
+   
+   #endregion
+   
+   public static explicit operator string(Name name) => name.Value;
+
+   public static explicit operator Name(string nameString) => new (nameString);
+   
+   public static bool operator ==(Name n1, Name n2) => n1.Value.Equals(n2.Value);
+
+   public static bool operator !=(Name n1, Name n2) => !n1.Value.Equals(n2.Value);
+}
