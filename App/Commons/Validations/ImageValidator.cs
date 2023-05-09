52d31b7b931733c99a53226646a2f1dd8ddb1269
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-09 10:07:17 +0700

    feat<domain>: create `ImageValidator`

---
+++ App/Commons/Validations/ImageValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+
+namespace App.Commons.Validations;
+
+public class ImageValidator : AbstractValidator<IFormFile>
+{
+   public ImageValidator()
+   {
+      RuleFor(img => img.ContentType)
+         .Must(fileType =>
+            fileType.Equals("image/jpeg") ||
+            fileType.Equals("image/jpg") ||
+            fileType.Equals("image/png")
+         );
+   }
+}
