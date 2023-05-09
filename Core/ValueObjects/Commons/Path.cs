63d4b35294b8fb74e14bc9f582f0ac9df6903d7b
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-09 07:16:06 +0700

    feat<core>: create `Path` value object

---
+++ Core/ValueObjects/Commons/Path.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Core.ValueObjects.Commons;
+
+public readonly struct Path
+{
+   public string Value { get; }
+
+   public Path(string value) => Value = value;
+
+   #region Static
+
+   /// <summary>
+   /// Validates the URI value.
+   /// </summary>
+   public static readonly Regex UriRegex = new(@"^(https?|ftp)://[^\s/$.?#].[^\s]*$");
+
+   public static bool IsValid(string value) => UriRegex.IsMatch(value);
+
+   #endregion
+
+   public static explicit operator string(Path path) => path.Value;
+
+   public static explicit operator Path(string pathString) => new(pathString);
+
+   public static bool operator ==(Path p1, Path p2) => p1.Value.Equals(p2.Value);
+
+   public static bool operator !=(Path p1, Path p2) => !p1.Value.Equals(p2.Value);
+}
