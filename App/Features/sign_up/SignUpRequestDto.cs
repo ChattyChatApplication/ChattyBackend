96032cf05ecd35f33793e51947f0ffc3ecc3664a
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-12 12:36:10 +0700

    feat<features>: require user info in `SignUpFeat`

--- App/Features/sign_up/SignUpRequestDto.cs
+++ App/Features/sign_up/SignUpRequestDto.cs
@@ -1,8 +1,12 @@
-﻿namespace App.Features;
+﻿using Microsoft.AspNetCore.Http;
+
+namespace App.Features;
 
 public class SignUpRequestDto
 {
    public string Username { get; set; } = null!;
    public string Email { get; set; } = null!;
    public string Password { get; set; } = null!;
+   public string Fullname { get; set; } = null!;
+   public IFormFile Avatar { get; set; } = null!;
 }
