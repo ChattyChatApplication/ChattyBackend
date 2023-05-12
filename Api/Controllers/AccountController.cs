b3b53af8fb52b0595b30858fd4a239ed2bcb4699
vuphamlethanh <vuphamlethanh.public@gmail.com>
2023-05-12 12:35:09 +0700

    feat<api>: create `AccountController` within `SignUpAsync` func

---
+++ Api/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using App.Features;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[Route("api/accounts")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+   private readonly ISignUpFeat _signUpFeat;
+
+   public AccountController(ISignUpFeat signUpFeat)
+   {
+      _signUpFeat = signUpFeat;
+   }
+   
+   [HttpPost]
+   public async Task<IActionResult> SignUpAsync([FromForm] SignUpRequestDto signUpRequest)
+   {
+      try
+      {
+         var authToken = await _signUpFeat.HandleAsync(signUpRequest);
+         return StatusCode(201, new
+         {
+            authToken
+         });
+      }
+      catch (ValidationException e)
+      {
+         return BadRequest(e.Errors);
+      }
+   }
+}
